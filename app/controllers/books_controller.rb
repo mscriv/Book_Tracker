484976fe282720a337d7b93217ec68ca5b6fae5d
Michael Scriven <bigscriv03@gmail.com>
2014-10-31 14:27:14 -0400

    Added Kamaniri gem, modified index

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,7 +4,8 @@ class BooksController < ApplicationController
   # GET /books
   # GET /books.json
   def index
-    @books = Book.all
+    @books = Book.order("author").page(params[:page]).per(10)
+
   end
 
   # GET /books/1
