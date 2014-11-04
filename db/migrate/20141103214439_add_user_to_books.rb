ed1c2a9bde3dc94707fa093e2201a130f7db0c13
Michael Scriven <bigscriv03@gmail.com>
2014-11-04 07:26:51 -0500

    modified add user to books migration

--- db/migrate/20141103214439_add_user_to_books.rb
+++ db/migrate/20141103214439_add_user_to_books.rb
@@ -1,6 +1,8 @@
 class AddUserToBooks < ActiveRecord::Migration
   def change
     add_reference :books, :user, index: true
+    user = User.create(name: 'Michael Scriven', email: 'mike@hotmail.com')
+
     Book.update_all "user_id = #{user.id}"
 
   end
