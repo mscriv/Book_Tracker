76ff3a6f09821acf94b5aca1fffba57da018b61d
Michael Scriven <bigscriv03@gmail.com>
2014-11-12 18:08:06 -0500

    Modified sessions controller

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -9,14 +9,13 @@ class SessionController < ApplicationController
                   email: params[:email],
                   password: params[:password],
                   password_confirmation: params[:password_confirmation])
+
     if user.save
       session[:user_id] = user.id
       flash[:notice] = 'You have successfully signed up.'
     else
       flash[:error] = "We were unable to sign you up. #{user.errors.full_messages.join('. ')}."
-
     end
-  redirect_to root_path
 
   end
 
